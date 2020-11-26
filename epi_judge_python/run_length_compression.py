fad1f1af60b72059c71b4b718a7a36fff0e7fba4
Michael Oyibo <mcvavy@gmail.com>
2020-11-26 11:35:18 +0000

    Solved to up 6.12

--- epi_judge_python/run_length_compression.py
+++ epi_judge_python/run_length_compression.py
@@ -1,15 +1,30 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
-
 def decoding(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    count = 0; result = []
+    
+    for c in s:
+        if c.isdigit():
+            count = count * 10 + int(c)
+        else:
+            result.append(c * count)
+            count = 0
+    return ''.join(result)
 
 
 def encoding(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    res = ""
+    i = 0
+    while i < len(s):
+        count = 1
+        j = i
+        while j < len(s) - 1 and s[j] == s[j + 1]:
+            count += 1
+            j += 1
+        res += str(count) + s[j]
+        i = j + 1
+    return res
 
 
 def rle_tester(encoded, decoded):
--- problem_mapping.js
+++ problem_mapping.js
@@ -616,7 +616,7 @@ problem_mapping = {
                 "total": 500
             },
             "Python: snake_string.py": {
-                "passed": 0,
+                "passed": 500,
                 "total": 500
             }
         },
@@ -630,7 +630,7 @@ problem_mapping = {
                 "total": 200
             },
             "Python: run_length_compression.py": {
-                "passed": 0,
+                "passed": 200,
                 "total": 200
             }
         },
