100b668cac622d17bf2b0c2457d8c3063267e056
Michael oyibo <michael.oyibo@runpath.com>
2021-03-09 07:20:49 +0000

    updated solution

--- epi_judge_python/knapsack.py
+++ epi_judge_python/knapsack.py
@@ -7,10 +7,31 @@ from test_framework.test_utils import enable_executor_hook
 
 Item = collections.namedtuple('Item', ('weight', 'value'))
 
-
 def optimum_subject_to_capacity(items: List[Item], capacity: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    items:       1,  2,  3, ... n
+    item value: v1, v2, v3, ... v_n
+    
+    +---+---+---+
+    |w0 |w1 | w2|
+    +---+---+---+
+    | a | b | c |
+    +---+---+---+
+    | d | e | f |
+    +---+---+---+
+    """
+    n = len(items)
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+    
+    for row in range(1, n + 1):
+        for col in range(capacity + 1):
+            if row == 0 or col == 0:
+                dp[row][col] == 0
+            elif items[row - 1][0] > col:
+                dp[row][col] = dp[row-1][col]
+            else:
+                dp[row][col] = max(dp[row-1][col], items[row - 1][1] + dp[row - 1][col - items[row-1][0]])
+    return dp[n][capacity]
 
 
 @enable_executor_hook
